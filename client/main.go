bdf26e8d9d30915f05244d726c4dde7f1264a50f
Valentina <70960916+ValentinaArb@users.noreply.github.com>
2025-03-27 17:22:08 -0300

    tira error

--- client/main.go
+++ client/main.go
@@ -38,34 +38,15 @@ func main() {
 	
 	log.Println("Salida del programa.")
 
-
-	/* utils.LeerConsola() */
-
-	/* 
-	func LeerConsola() {
-	reader := bufio.NewReader(os.Stdin)
-	log.Println("Ingrese los mensajes")
-	text, _ := reader.ReadString('\n')
-	log.Print(text)
-}
-	*/
-
-
-/* 	utils.LeerConsola() */
-
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
-	/* // enviar un mensaje al servidor con el valor de la config
+	// enviar un mensaje al servidor con el valor de la config
 	utils.EnviarMensaje(globals.ClientConfig.Ip, globals.ClientConfig.Puerto, globals.ClientConfig.Mensaje)
 
 	// leer de la consola el mensaje
 	utils.LeerConsola()
 	log.Println("Enviando mensaje al servidor...")
 
-	
 	// generamos un paquete y lo enviamos al servidor
 	utils.GenerarYEnviarPaquete()
- */
-
-
 }
